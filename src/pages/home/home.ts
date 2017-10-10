ce11c157d7a7e3f4e7f6216e4c1d3d0783e6973c
junho7 <junho7@gmail.com>
2017-10-09 23:34:19 -0700

    working_device

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { Camera } from '@ionic-native/camera';
-// import { NativeStorage } from '@ionic-native/native-storage';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 @Component({
   selector: 'page-home',
@@ -14,21 +14,21 @@ export class HomePage {
   constructor(public navCtrl: NavController
     , public platform: Platform
     , private camera: Camera
-    // , private nativeStorage: NativeStorage
+    , private nativeStorage: NativeStorage
   ) {
     this.base64Image = new Array();
 
-    // this.platform.ready().then(() => {
-    //   this.nativeStorage.getItem('photos').then(data => {
+    this.platform.ready().then(() => {
+      this.nativeStorage.getItem('photos').then(data => {
         
-    //     // data = data.replace(/^file:\/\//, '');
-    //     console.log(data);
-    //     this.base64Image = data.split(', ');
-    //   },
-    //     error => {
-    //       console.log("error in getting photos " + error);
-    //     });
-    // });
+        // data = data.replace(/^file:\/\//, '');
+        console.log(data);
+        this.base64Image = data.split(', ');
+      },
+        error => {
+          console.log("error in getting photos " + error);
+        });
+    });
   }
 
   takePicture() {
@@ -47,10 +47,10 @@ export class HomePage {
       let imageUris = this.base64Image.map(o => o).join(', ');
       // let imageUris = this.base64Image.join(', ');
 
-      // this.nativeStorage.setItem('photos', imageUris).then(
-      //   () => console.log('Stored item!'),
-      //   error => console.error('Error storing item', error)
-      // );
+      this.nativeStorage.setItem('photos', imageUris).then(
+        () => console.log('Stored item!'),
+        error => console.error('Error storing item', error)
+      );
       console.log("imageUris is " + imageUris);
 
     }, (err) => {
